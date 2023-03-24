ac0c0e304ce63f2b1020e90eb3e49360118ad34c
izumiChan16 <c2403974446@gmail.com>
2023-03-24 09:04:20 +0800

    添加了cJSON库，为users.h添加注释

--- reader_information/readers_borrowing.h
+++ reader_information/readers_borrowing.h
@@ -5,6 +5,27 @@
 #ifndef LIBRARY_MANAGEMENT_READERS_BORROWING_H
 #define LIBRARY_MANAGEMENT_READERS_BORROWING_H
 
+/**
+ * @FunctionName ReadersInfo
+ * @CreateTime 2023/3/24 8:49
+ * @Author izumi
+ * TODO: 生成存储信息的结构体
+ */
+
+typedef struct ReadersInfo{
+    char userid[12];
+    char username[20];
+    char contact[12];
+} ReadersInfo;
+
+/**
+ * @FunctionName ReadersTable
+ * @CreateTime 2023/3/24 8:50
+ * @Author izumi
+ * TODO: 存储 ReadersInfo 的结构体
+ */
+
+typedef struct ReadersTable
 
 
 #endif //LIBRARY_MANAGEMENT_READERS_BORROWING_H
--- user_information/user.h
+++ user_information/user.h
@@ -25,24 +25,34 @@ typedef struct UserTable {
 } UserTable;
 
 
+// 初始化用户表
 UserTable initUserTable();
 
+//读取文件，参数传入整个用户表和文件名（用来打开文件）
 UserTable readFile(UserTable table, const char *filename);
 
+// 添加用户，往用户表里添加用户，参数传入各种信息
 UserTable addUser(UserTable table, const char *userid, const char *password, const char *username, const char *contact, const char *is_admin);
 
+// 通过ID查找用户
 int findUserById(UserTable table, const char *userid);
 
+// 通过用户名查找用户
 int findUserByUsername(UserTable table, const char *username);
 
+// 通过ID删除用户
 UserTable deleteUserById(UserTable table, const char *userid);
 
+// 通过用户名删除用户
 UserTable deleteUserByUsername(UserTable table, const char *username);
 
+// 通过ID修改用户
 UserTable modifyUserById(UserTable table, const char *userid, const char *newPassword, const char *newUsername, const char *newContact, const char *newIsAdmin) ;
 
+// 通过用户名修改用户
 UserTable modifyUserByUsername(UserTable table, const char *username, const char *newPassword, const char *newUserId, const char *newContact, const char *newIsAdmin);
 
+// 最后保存文件
 void saveFile(UserTable table, const char *filename) ;
 
 #endif
