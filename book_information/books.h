e3dfef0d1afd68e1f5a823bf8cd950a55bf05ae7
zassdasd <2170551146@qq.com>
2023-03-31 09:15:24 +0800

    wanmei

--- book_information/books.h
+++ book_information/books.h
@@ -19,30 +19,24 @@ typedef struct BooksInfo{
     char num_copies[20];
 //    该编号图书总数
     char num_available[20];
-//    剩余书数
 }BooksInfo;
 
-typedef struct BooksTable{
+typedef struct BookTable{
     struct BooksInfo BooksInfo[100];
     int bookCount;
-}BooksTable;
+}BookTable;
 
-BooksTable initBooksTable();
+BookTable initBookTable();
 //初始化图书表
 
-BooksTable readFile(BooksTable table, const char *books);
+BookTable readFile(BookTable table, const char *filename);
 //打开由“文件名”指定的文件fscanf.文件的每一行应包含格式为“ISBN，书籍名称，作者，出版商，价格，份数，可用数量”的书籍数据
 
-BooksTable addBook(BooksTable table, const char *isbn, const char *bookname, const char *writer, const char *publisher, const char *price, const char *num_copies, const char *num_available);
+BookTable addBook(BookTable table, const char *isbn, const char *bookname, const char *writer, const char *publisher, const char *price, const char *num_copies, const char *num_available);
 //
 
-int findISBN(BooksTable table, const char *isbn);
+int findISBN(BookTable table, const char *isbn);
 //循环遍历对象数组中的每本书，并使用 将每本书的 ISBN 与给定字符串进行比较。如果找到匹配的书籍，该函数将返回其在数组中的索引
 
-)int findBookByBookName(struct BooksTable table, const char *bookname;
+int findBookByBookName(BookTable table, const char *bookname);
 //通过书名查找书籍
-
-BooksInfo deleteISBN(BooksTable table, const char *ISBN);
-//通过ISBN删除书籍
-
-BooksInfo deleteBookByBookName(BooksTable table, const char *bookname);
