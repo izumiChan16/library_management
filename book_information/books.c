022f77f573b690be9ca71e9dd06a37160c4581f0
zassdasd <2170551146@qq.com>
2023-04-03 14:00:40 +0800

    4.3

--- book_information/books.c
+++ book_information/books.c
@@ -16,14 +16,14 @@ BookTable readFile(BookTable table, const char *books) {
         return table;
     }
 //    判断file是否为NULL。如果file为NULL，表示文件打开失败，这时会执行if代码块中的语句，打印一个错误消息并返回table。
-    while (fscanf(file, "%[^,],%[^,],%[^,],%[^,],%d,%d,%d\n",
+    while (fscanf(file, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^\n]",
                   table.BooksInfo[table.bookCount].ISBN,
                   table.BooksInfo[table.bookCount].Bookname,
                   table.BooksInfo[table.bookCount].Writter,
                   table.BooksInfo[table.bookCount].Publisher,
                   table.BooksInfo[table.bookCount].Price,
-                  &table.BooksInfo[table.bookCount].num_copies,
-                  &table.BooksInfo[table.bookCount].num_available) != EOF) {
+                  table.BooksInfo[table.bookCount].Total,
+                  table.BooksInfo[table.bookCount].Available) != EOF) {
         table.bookCount++;
     }
 //    从文件中读取一行数据并解析为书籍信息，直到文件结束。同时，每读取一行数据就会递增table.bookCount，用于记录当前已经读取了多少本书。
@@ -31,18 +31,18 @@ BookTable readFile(BookTable table, const char *books) {
     return table;
 }
 
-BookTable addBook(BookTable table, const char *isbn, const char *bookname, const char *writer, const char *publisher, const char *price, const char *num_copies, const char *num_available) {
-    if (table.bookCount >= num_copies) {
+BookTable addBook(BookTable table, const char *isbn, const char *Bookname, const char *writer, const char *publisher, const char *price, const char *Total, const char *Available) {
+    if (table.bookCount >= 100) {
         printf("Book table is full.\n");
         return table;
     }
     strcpy(table.BooksInfo[table.bookCount].ISBN, isbn);
-    strcpy(table.BooksInfo[table.bookCount].Bookname, bookname);
+    strcpy(table.BooksInfo[table.bookCount].Bookname, Bookname);
     strcpy(table.BooksInfo[table.bookCount].Writter, writer);
     strcpy(table.BooksInfo[table.bookCount].Publisher, publisher);
     strcpy(table.BooksInfo[table.bookCount].Price, price);
-    strcpy(table.BooksInfo[table.bookCount].num_copies, num_copies);
-    strcpy(table.BooksInfo[table.bookCount].num_available, num_available);
+    strcpy(table.BooksInfo[table.bookCount].Total, Total);
+    strcpy(table.BooksInfo[table.bookCount].Available, Available);
     table.bookCount++;
     return table;
 }
@@ -60,13 +60,71 @@ int findISBN(BookTable table, const char *isbn) {
     return -1;
 }
 
-int findBookByBookName(BookTable table, const char *bookname) {
-    for (int i = 0; i < table.BooksInfo[i].num_copies; i++) {
-        if (strcmp(table.BooksInfo[i].Bookname, bookname) == 0) {
+int findBookByBookname(BookTable table, const char *Bookname) {
+    for (int i = 0; i < table.BooksInfo[i].Total; i++) {
+        if (strcmp(table.BooksInfo[i].Bookname, Bookname) == 0) {
             return i;
         }
     }
     return -1;
 }
 
+BookTable deleteBookByBookname(BookTable table, const char *Bookname) {
+    int index = findBookByBookname(table, Bookname);
+    if (index == -1) {
+        printf("Book not found.\n");
+        return table;
+    }
+    for (int i = index; i < table.bookCount - 1; i++) {
+        table.BooksInfo[i] = table.BooksInfo[i + 1];
+    }
+    table.bookCount--;
+    return table;
+}
 
+BookTable modifyBookByISBN(BookTable table, const char *ISBN, const char *newPrice, const char *newTotal, const char *newAvailable) {
+    int index = findISBN(table, ISBN);
+    if (index == -1) {
+        printf("Book not found.\n");
+        return table;
+    }
+//    strcpy(table.BooksInfo[index].Publisher, newPublisher);
+//    出版社与ISBN码绑定，所以不能通过ISBN码修改
+    strcpy(table.BooksInfo[index].Price, newPrice);
+    strcpy(table.BooksInfo[index].Total, newTotal);
+    strcpy(table.BooksInfo[index].Available, newAvailable);
+    return table;
+};
+
+BookTable modifyBookByBookname(BookTable table, const char *Bookname, const char *newPrice, const char *newTotal, const char *newAvailable) {
+    int index = findBookByBookname(table, Bookname);
+    if (index == -1) {
+        printf("Book not found.\n");
+        return table;
+    }
+    strcpy(table.BooksInfo[index].Price, newPrice);
+    strcpy(table.BooksInfo[index].Total, newTotal);
+    strcpy(table.BooksInfo[index].Available, newAvailable);
+    return table;
+}
+
+void saveFile(BookTable table, const char *books) {
+    FILE *file = fopen(books, "w");
+    if (!file) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    for (int i = 0; i < table.bookCount; i++) {
+        fprintf(file, "%s,%s,%s,%s,%s\n",
+                table.BooksInfo[i].Bookname,
+                table.BooksInfo[i].Writter,
+                table.BooksInfo[i].Publisher,
+                table.BooksInfo[i].ISBN,
+                table.BooksInfo[i].Price,
+                table.BooksInfo[i].Total,
+                table.BooksInfo[i].Available);
+    }
+
+    fclose(file);
+}
--- book_information/books.h
+++ book_information/books.h
@@ -5,7 +5,6 @@
 #ifndef LIBRARY_MANAGEMENT_BOOKS_H
 #define LIBRARY_MANAGEMENT_BOOKS_H
 
-#endif //LIBRARY_MANAGEMENT_BOOKS_H
 
 typedef struct BooksInfo{
     char Bookname[20];
@@ -16,14 +15,15 @@ typedef struct BooksInfo{
     char ISBN[20];
     char Price[20];
 //    价格
-    char num_copies[20];
+    char Total[20];
 //    该编号图书总数
-    char num_available[20];
+    char Available[20];
 }BooksInfo;
 
 typedef struct BookTable{
     struct BooksInfo BooksInfo[100];
     int bookCount;
+//    书种类的个数
 }BookTable;
 
 BookTable initBookTable();
@@ -32,11 +32,25 @@ BookTable initBookTable();
 BookTable readFile(BookTable table, const char *filename);
 //打开由“文件名”指定的文件fscanf.文件的每一行应包含格式为“ISBN，书籍名称，作者，出版商，价格，份数，可用数量”的书籍数据
 
-BookTable addBook(BookTable table, const char *isbn, const char *bookname, const char *writer, const char *publisher, const char *price, const char *num_copies, const char *num_available);
+BookTable addBook(BookTable table, const char *isbn, const char *Bookname, const char *writer, const char *publisher, const char *price, const char *Total, const char *Available);
 //
 
 int findISBN(BookTable table, const char *isbn);
 //循环遍历对象数组中的每本书，并使用 将每本书的 ISBN 与给定字符串进行比较。如果找到匹配的书籍，该函数将返回其在数组中的索引
 
-int findBookByBookName(BookTable table, const char *bookname);
+int findBookByBookname(BookTable table, const char *Bookname);
 //通过书名查找书籍
+
+BookTable deleteBookByBookname(BookTable table, const char *Bookname);
+//通过书名删除书籍
+
+BookTable modifyBookByISBN(BookTable table, const char *ISBN, const char *newPrice, const char *newTotal, const char *newAvailable) ;
+//通过ISBN码修改，因为ISBN码与书名，作者,出版社高度绑定所以只能改 总数，可用数，价格
+
+BookTable modifyBookByBookname(BookTable table, const char *Bookname, const char *newPrice, const char *newTotal, const char *newAvailable) ;
+//
+
+void saveFile(BookTable table, const char *books) ;
+//保存文件
+
+#endif
